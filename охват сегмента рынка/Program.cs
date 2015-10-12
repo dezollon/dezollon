daaf0a9433f6e4db187af0ae9ab0196a6963b65e
dezollon <dezollon@yandex.ru>
2015-10-12 18:00:34 +0300

    начало

---
+++ охват сегмента рынка/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace охват_сегмента_рынка
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var b = 12;
+            var a = 12;
+
+        }
+    }
+}
